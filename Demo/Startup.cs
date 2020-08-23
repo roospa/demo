0bc08a90f95c7c972d115c25766cf0dfce5e6385
Patric Roos <roos.patric@gmail.com>
2020-08-23 09:14:29 +0000

    Updated message again

--- Demo/Startup.cs
+++ Demo/Startup.cs
@@ -32,7 +32,7 @@ namespace Demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Azure!");
+                    await context.Response.WriteAsync("Hello again, Azure!");
                 });
             });
         }
